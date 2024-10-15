d06c9615ec73e08b217d9d9ca6602bb67841e78e
Aaron Endsley <aaronendsley@gmail.com>
2024-10-15 18:47:00 -0500

    Add Styles and the ability to generate a grid

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AE-Odin-Project-Etch-a-Sketch
+
+This is a project I have built that was part of the foundations curriculum of the Odin Project
+You can view it 
+https://www.theodinproject.com/lessons/foundations-etch-a-sketch
---
+++ app.js
@@ -0,0 +1,39 @@
+//Create Square and return the Square
+function createSquare() {
+  const NEWSQUARE = document.createElement("div");
+  NEWSQUARE.className = "square";
+  return NEWSQUARE;
+}
+
+//Create a Row add squares to it and return the Row
+function createRow(NumberOfSquares) {
+  const NEWROW = document.createElement("div");
+  NEWROW.className = "row";
+
+  //create and append squares
+  for (let i = 1; i <= NumberOfSquares; i++) {
+    NEWROW.appendChild(createSquare());
+
+    if (i >= NumberOfSquares) {
+      return NEWROW;
+    }
+  }
+}
+
+//Create the Grid
+function createGrid(rowsAndSquares) {
+  const CONTAINER = document.getElementById("container");
+
+  //Only allow the user to make 100 rows and 100 Squares in each row
+  if (rowsAndSquares <= 100) {
+    // Check if the Container Has Children and remove them;
+    if (CONTAINER.hasChildNodes) {
+      CONTAINER.replaceChildren();
+    }
+
+    //create rows
+    for (let i = 1; i <= rowsAndSquares; i++) {
+      CONTAINER.appendChild(createRow(rowsAndSquares));
+    }
+  }
+}
